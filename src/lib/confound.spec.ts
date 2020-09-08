e6e36992599f63bf098f6242b0ff49a1e91253ba
Mark Tranter <mark.a.tranter@gmail.com>
2020-09-08 10:26:57 +1000

    Build library

---
+++ src/lib/confound.spec.ts
@@ -0,0 +1,65 @@
+import { ConfigValueSource, ConfigValueSources } from './confound'
+
+const { envOrDie, obj } = ConfigValueSources
+
+interface NestedConfig {
+  age: number
+}
+
+interface MyConfig {
+  name: string,
+  nested: NestedConfig
+}
+
+describe("Confound", () => {
+
+  beforeEach(() => {
+    Object.keys(process.env).filter(e => e.startsWith("CONFOUND_")).forEach(e => delete process.env[e])
+  })
+
+  it("Should load Config from literals", async () => {
+    const configSource: ConfigValueSource<MyConfig> =
+      ConfigValueSources.obj<MyConfig>({
+        name: "Confound",
+        nested: {
+          age: 1
+        }
+      })
+    const config: MyConfig = await configSource()
+    expect(config).toEqual({
+      name: "Confound",
+      nested: {
+        age: 1
+      }
+    })
+  })
+  it("Should load Config from the environment", async () => {
+    process.env["CONFOUND_NAME"] = "Confound"
+    process.env["CONFOUND_AGE"] = "1"
+
+    const configSource: ConfigValueSource<MyConfig> = obj<MyConfig>({
+      name: envOrDie("CONFOUND_NAME"),
+      nested: obj({
+        age: envOrDie("CONFOUND_AGE").map(parseInt)
+      })
+    })
+    const config = await configSource()
+    expect(config).toEqual({
+      name: "Confound",
+      nested: {
+        age: 1
+      }
+    })
+  })
+  it("Should blow up on missing env variable", async () => {
+    process.env["CONFOUND_NAME"] = "Confound"
+    const configSource: ConfigValueSource<MyConfig> = obj<MyConfig>({
+      name: envOrDie("CONFOUND_NAME"),
+      nested: obj({
+        age: envOrDie("CONFOUND_AGE").map(s => parseInt(s))
+      })
+    })
+    const config = configSource()
+    await expect(config).rejects.toEqual("Expected env var CONFOUND_AGE")
+  })
+})
---
+++ src/lib/confound.ts
@@ -0,0 +1,41 @@
+
+
+export type ConfigFetcher<T> = () => Promise<T>
+
+export type ConfigValueSource<T> = ConfigFetcher<T> & {
+  map: <S>(f: (t: T) => S | Promise<S>) => ConfigValueSource<S>
+  flatMap: <S>(f: (t: T) => ConfigValueSource<S>) => ConfigValueSource<S>
+}
+
+const configValueSource: <T>(cf: ConfigFetcher<T>) => ConfigValueSource<T> = <T>(cf: ConfigFetcher<T>) => Object.assign(() => cf(), {
+  map: <S>(f: (t: T) => S | Promise<S>): ConfigValueSource<S> => configValueSource<S>(() => cf().then(f)),
+  flatMap: <S>(f: (t: T) => ConfigValueSource<S>): ConfigValueSource<S> => configValueSource<S>(() => cf().then(r => f(r)()))
+})
+
+interface ConfigValueSources {
+  of: <T>(cf: () => Promise<T>) => ConfigValueSource<T>
+  lit: <T>(t: T | Promise<T>) => ConfigValueSource<T>
+  env: (n: string) => ConfigValueSource<string | undefined>
+  orDie: <T>(cvs: ConfigValueSource<T | undefined>, failureMsg: string) => ConfigValueSource<T>
+  envOrDie: (e: string) => ConfigValueSource<string>
+  obj: <T>(config: { [k in keyof T]: ConfigValueSource<T[k]> | T[k] }) => ConfigValueSource<T>
+}
+
+export const ConfigValueSources: ConfigValueSources = {
+  of: <T>(cf: () => Promise<T>) => configValueSource(cf),
+  lit: <T>(t: T | Promise<T>) => configValueSource(() => Promise.resolve(t)),
+  env: (n: string) => configValueSource(() => Promise.resolve(process.env[n])),
+  orDie: <T>(cvs: ConfigValueSource<T | undefined>, failureMsg: string) => cvs.map(s => s ? Promise.resolve(s) : Promise.reject<T>(failureMsg)),
+  envOrDie: (e: string) => ConfigValueSources.orDie(ConfigValueSources.env(e), `Expected env var ${e}`),
+  obj: <T>(config: { [k in keyof T]: ConfigValueSource<T[k]> | T[k] }) => {
+    const configAny = config as { [k: string]: ConfigValueSource<any> }
+    const getValue = (src: any) => src instanceof Function ? src() : Promise.resolve(src)
+    const fetchedP = Object.keys(config).reduce((p, n) => ([...p, getValue(configAny[n]).then((v: any) => ({ [n]: v }))]), [] as Array<Promise<any>>)
+    const result = Promise.all(fetchedP).then(fetched => {
+      return fetched.reduce((p, n) => Object.assign({}, p, n), {})
+    })
+
+    return ConfigValueSources.lit(result as Promise<T>)
+  }
+}
+
